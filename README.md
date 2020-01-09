6c055f91902d8e297fdc582c12c63174d373969d
mninc <manicminer106@gmail.com>
2020-01-09 21:43:45 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# jstf
+
+An API wrapper for some TF2-related sites. Created by [manic](http://manic.tf/). Visit the original python version [here](https://github.com/mninc/pytf).
+
+These methods are designed so I can use this module rather than pasting functions into many blocks of code. It is not intended to be an API wrapper for every tf2 site. If you want to add some methods, feel free to make a pull request.
+
---
+++ index.js
@@ -0,0 +1,101 @@
+const httpRequest = require("request");
+
+class Manager {
+    userToken;
+    apiKey;
+
+    /**
+     * @param {string} apiKey
+     * @param {function} callback
+     */
+    constructor(apiKey, callback) {
+        this.apiKey = apiKey;
+        this.bpGetUserToken(function(err, token) {
+            if (err) callback(err);
+            else {
+                this.userToken = token;
+                callback();
+            }
+        })
+    }
+
+    /**
+     *
+     * @param {Object} requestOptions
+     * @param {String} requestOptions.url
+     * @param {String} requestOptions.method
+     * @param {Object} requestOptions.qs
+     * @param {Object} options
+     * @param {boolean} [options.doNotParse]
+     * @param {function} callback
+     */
+    request(requestOptions, options, callback) {
+        httpRequest(requestOptions, function(err, response, body) {
+            if (err) callback(err);
+            else if (options.doNotParse) callback(err, body);
+            else callback(err, JSON.parse(body));
+        })
+    }
+
+    /**
+     * @param {function} callback
+     */
+    bpGetUserToken(callback) {
+        if (!this.apiKey) callback("this.apiKey not set");
+        this.request(
+            {
+                url: "https://backpack.tf/api/aux/token/v1",
+                method: "GET",
+                qs: {
+                    key: this.apiKey
+                }
+            },
+            {},
+            function(err, body) {
+                if (err) callback(err);
+                else if (body.message) callback(body.message);
+                else callback(err, body.token);
+            }
+        )
+    }
+
+    /**
+     *
+     * @param {array} listings
+     * @param {boolean} parse
+     * @param {function} callback
+     */
+    bpCreateListings(listings, parse, callback) {
+        this.request(
+            {
+                url: "https://backpack.tf/api/classifieds/list/v1",
+                method: "POST",
+                json: {
+                    token: this.userToken,
+                    listings: listings
+                }
+            },
+            {},
+            function(err, body) {
+                if (err) callback(err);
+                else if (!parse) callback(err, body);
+                else {
+                    let response = {
+                        successful: [],
+                        unsuccsessful: {}
+                    };
+
+                    if (!body.listings) return callback(err, response);
+                    for (let item in body.listings) {
+                        if (body.listings.hasOwnProperty(item)) {
+                            let success = body.listings[item];
+                            if (success.created) response.successful.push(item);
+                            else response.unsuccsessful[item] = success.error;
+                        }
+                    }
+                    callback(err, response);
+                }
+            }
+        )
+    }
+}
